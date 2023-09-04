f5f0b5d87eb31e7f4b0bab6d6a1130a386e3c47c
William Rocha <wiilliamdroocha@gmail.com>
2023-09-04 17:16:38 -0300

    controller pitches

---
+++ app/controllers/pitches_controller.rb
@@ -0,0 +1,64 @@
+class PitchesController < ApplicationController
+  before_action :set_pitch, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @pitches = Pet.all
+    @pitches = Pet.search_pets(params[:query]) if params[:query].present?
+  end
+
+  def show
+    @pitch = Pitch.new
+    @pitchs_users = Pitch.where(pitch: @pitch)
+    @pitchs_count = Pitch.where(pitch: @pitch, owner: current_user).count
+    @pitchs_approved = Pitch.where(pitch: @pitch, status: true).count
+  end
+
+  # GET /pitches/new
+  def new
+    @pitch = Pet.new
+  end
+
+  # GET /pitches/1/edit
+  def edit
+  end
+
+  # POST /pitches
+  def create
+    @pitch = Pitch.new(pet_params)
+    @pitch.user_id = current_user
+
+    if @pitch.save
+      redirect_to @pitch, notice: "Pitch was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /pitches/1
+  def update
+    if @pitch.update(pet_params)
+      redirect_to @pitch, notice: "Pitch was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /pitches/1
+  def destroy
+    @pitch.destroy
+    redirect_to pets_url, notice: "Pitch was successfully destroyed.", status: :see_other
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pitch
+    @pitch = Pitch.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def pitch_params
+    params.require(:pitch).permit(:title, :languages, :category, :description, :source)
+  end
+end
