3782fbf750ca5a77a4999030f2060dad190394f0
William Rocha <wiilliamdroocha@gmail.com>
2023-09-05 17:09:52 -0300

    seed alimentado

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :pitch, dependent: :destroy
-  belongs_to :user, dependent: :destroy
+  belongs_to :pitch
+  belongs_to :user
 
   validates :content, length: { minimum: 6 }
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
