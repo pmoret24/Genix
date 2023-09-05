afa12719ffe5b977459c8cd88a39712a91230797
Pedro Moretzsohn <p.moretzsohn24@gmail.com>
2023-09-05 18:12:56 -0300

    fixed seeds

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,6 +3,5 @@ class Chatroom < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
-  validates :sender, presence: true
-  validates :receiver, presence: true
+
 end
