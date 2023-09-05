43799c8b1f9f6b86bf28dbd4cf3ea83c9d1f3473
Pedro Moretzsohn <p.moretzsohn24@gmail.com>
2023-09-05 17:39:47 -0300

    pending

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,7 @@ class Chatroom < ApplicationRecord
   has_many :messages
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  validates :sender, presence: true
+  validates :receiver, presence: true
 end
