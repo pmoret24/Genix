fd779afee79669ca3530674a5491a0f953f6695e
Luis Macedo <lfelipe_macedo@hotmail.com>
2023-09-06 17:57:29 -0300

    melhorias no profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,8 @@ class ProfilesController < ApplicationController
     @connection = Connection.new
 
     @pending_connections = Connection.where(status: false).where(friend: current_user)
-    @approved_connections = Connection.where(status: true).where(user: current_user).or(Connection.where(friend_id: current_user))
+    @approved_connections = Connection.where(status: true, user: current_user).or(Connection.where(status: true, friend: current_user))
+    # o acima é essa query otimizada @approved_connections = Connection.where(status: true).where(user: current_user).or(Connection.where(status: true).where(friend_id: current_user))
     @connection_two = Connection.where(
       '(user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)',
       current_user.id, @user.id, @user.id, current_user.id
