cd11a1784c89e45f93c70bd82cb639174fc1ee87
Filipe Mariano <filipemarianoc@gmail.com>
2023-09-06 16:06:38 -0300

    chat praticamente pronto

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,19 +3,26 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number,
+                    currentUserId: Number}
   static targets = ["messages"]
   connect() {
+    console.log(this.currentUserIdValue)
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.#insertMessageAndScrollDown(data)}
+      { received: data => this.#insertMessageAndScrollDown(data.html, data.sender_id)}
     )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
-  #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+  #insertMessageAndScrollDown(html, sender_id) {
+    if(this.currentUserIdValue != sender_id) {
+      html = html.replace("current-user", "other-user")
+    }
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", html)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+
   }
 
   resetForm(event) {
